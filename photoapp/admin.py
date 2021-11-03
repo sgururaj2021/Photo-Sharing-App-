e67fb34242df2cd8bc4debfcfa14142d1e814018
sgururaj2021 <gururaj1931@gmail.com>
2021-11-03 13:27:23 +0530

    First Commit

---
+++ photoapp/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Photo # We import the photo model
+
+# Register your models here.
+admin.site.register(Photo)
---
+++ users/views.py
@@ -0,0 +1,27 @@
+from django.views.generic import CreateView
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
+
+class SignUpView(CreateView): ...
+
+class CustomLoginView(LoginView):
+    template_name = 'users/login.html'
+
+class SignUpView(CreateView):
+    template_name = 'users/signup.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('photo:list')
+
+    def form_valid(self, form):
+        to_return = super().form_valid(form)
+
+        user = authenticate(
+            username=form.cleaned_data["username"],
+            password=form.cleaned_data["password1"],
+        )
+
+        login(self.request, user)
+
+        return to_return
